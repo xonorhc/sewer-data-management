efa8c1b6c983b3a88bbc6b22d3b135020a0eea3f
xonorhc <xonorhc@proton.me>
2025-03-31 19:32:35 -0300

    update

--- README.md
+++ README.md
@@ -2,49 +2,6 @@
 
 Modelo de dados espaciais para sistemas de esgotamento sanitario.
 
-## Itens
-
-- Ligacao de esgoto
-  - Tubulacao
-    - Ramal
-  - Unidade de Inspecao
-    - Caixa de Passagem
-    - Terminal de Inspecao e Limpeza
-  - Conexoes
-    - Curva
-    - Selim
-- Rede Coletora
-  - Tubulacao
-    - Coletor
-    - Coletor Tronco
-    - Interceptor
-  - Unidade de Inspecao
-    - Caixa de Passagem
-    - Poco de Inspecao
-    - Poco de Visita
-    - Terminal de Limpeza
-- Estação Elevatória de Esgoto Sanitário: Instalação que se destina ao transporte do esgoto do nível do poço de sucção das bombas ao nível de descarga na saída do recalque, acompanhando aproximadamente as variações da vazão afluente
-  - Bomba Centrifuga: São caracterizadas por possuírem um elementorotativo dotado de pá(rotor), que fornece aoliquido o trbalho mecânico para vencer o desnível. Composta em geral por quatro partes essenciais, são elas: carcaça, rotor ou punsionador, o eixo, a vedação e mancal.
-  - Poço de Sucção: Estrutura de transição que recebe as contribuições dos esgotos afluentes e as coloca à disposição das unidades de recalque.
-  - Tubulacao
-    - Linha de Recalque (LR): Tubulação de saída da EEE, tendo inicio após a última interligação do barrilete, opera em regime forçado
-  - Conexao
-    - Te
-    - Curva
-    - Ventosa
-  - Valvula
-    - Válvulas Gaveta: São utilizadas para isolar as linhas de suçcão e de recalque, nas ocasiões de manutenção das tubulações e equipamentos eletro-mecanicos da elevatória. Permitem boa vedação mesmo em altas pressoese, quando completamente abertas, oderecem pouca resistência à passagem do líquido.
-    - Vávulas de Retenção: Permitem apenas o escoamento do fluxo em uma direção e destinam-se à proteção das instalações de recalque contra o fluxo liquido.
-    - Válvulas Borboleta: Utilizadas para grandes diâmetros, as válvulas borboletas geralmente são mais econômicas do que as válvulas gaveta e requerem espaço menor instalação. Ocorre que sua vedação não é eficaz quanto válvula gaveta, especialmente em altas pressões.
-    - Vávula Flap: Utlizada em extrvasores por gravidade das elevatórias, a fim de evitar o refluxo da água nas ocasiões do nível máximo do corpo receptor, tendo o funcionamento semelhante ao da válvula de retenção.
-- Estacao Tratamento
-  - Tubulacao
-    - Emissario
-  - Unidades de Inspecao
-- Corpo Receptor
-  - Hidrografia
-  - Sistema de drenagem de aguas pluviais
-
 ### Fluxograma
 
 ```mermaid
--- src/create_tables.sql
+++ src/create_tables.sql
@@ -3,52 +3,20 @@ CREATE TABLE tipo_rede_esgoto (
     tipo varchar UNIQUE NOT NULL
 );
 
-INSERT INTO tipo_rede_esgoto
-    VALUES (1, 'Coletor'),
-    (2, 'Coletor tronco'),
-    (3, 'Interceptor'),
-    (4, 'Recalque'),
-    (5, 'Emissario');
-
 CREATE TABLE tipo_esgoto (
     id smallint PRIMARY KEY,
     tipo varchar UNIQUE NOT NULL
 );
-
-INSERT INTO tipo_esgoto
-    VALUES (1, 'Domestico'),
-    (2, 'Industrial'),
-    (3, 'Pluvial'),
-    (4, 'Tratado');
 
 CREATE TABLE tipo_material (
     id smallint PRIMARY KEY,
     tipo varchar UNIQUE NOT NULL
 );
 
-INSERT INTO tipo_material
-    VALUES (1, 'CA'),
-    (2, 'Concreto'),
-    (3, 'FoFo'),
-    (4, 'FGal'),
-    (5, 'PRFV'),
-    (6, 'PVC'),
-    (7, 'PVC Corrugado'),
-    (8, 'PVC DeFoFo'),
-    (9, 'PEAD'),
-    (10, 'PEAD Corrugado'),
-    (11, 'Aco'),
-    (12, 'Tijolo');
-
 CREATE TABLE tipo_situacao (
     id smallint PRIMARY KEY,
     tipo varchar UNIQUE NOT NULL
 );
-
-INSERT INTO tipo_situacao
-    VALUES (1, 'Ativa'),
-    (2, 'Inativa'),
-    (3, 'Em construcao');
 
 CREATE TABLE redes_esgoto (
     id serial PRIMARY KEY,
@@ -83,28 +51,10 @@ CREATE TABLE tipo_nivel_tratamento_esgoto (
     tipo varchar UNIQUE NOT NULL
 );
 
-INSERT INTO tipo_nivel_tratamento_esgoto
-    VALUES (1, 'Preliminar/Acondicionamento'),
-    (2, 'Primario'),
-    (3, 'Secundario'),
-    (4, 'Terciario');
-
 CREATE TABLE tipo_tecnologia_tratamento_esgoto (
     id smallint PRIMARY KEY,
     tipo varchar UNIQUE NOT NULL
 );
-
-INSERT INTO tipo_tecnologia_tratamento_esgoto
-    VALUES (1, 'Decantacao primaria'),
-    (2, 'Lagoa anaerobica'),
-    (3, 'Reator anaerobico'),
-    (4, 'Lagoa aerobica'),
-    (5, 'Lodos ativados'),
-    (6, 'Filtro biologico'),
-    (7, 'Coagulacao'),
-    (8, 'Flotacao'),
-    (9, 'Desinfeccao'),
-    (10, 'Outros - especificar na obs');
 
 CREATE TABLE estacoes_tratamento (
     id serial PRIMARY KEY,
@@ -137,14 +87,6 @@ CREATE TABLE tipo_bomba (
     tipo varchar UNIQUE NOT NULL
 );
 
-INSERT INTO tipo_bomba
-    VALUES (1, 'Centrifuga'),
-    (2, 'Submersa'),
-    (3, 'Submersivel'),
-    (4, 'Auto-aspirante'),
-    (5, 'Periferica'),
-    (6, 'Injetora');
-
 CREATE TABLE bombas (
     id serial PRIMARY KEY,
     geom GEOMETRY(point, 31984) UNIQUE NOT NULL,
@@ -166,11 +108,6 @@ CREATE TABLE tipo_forma (
     id smallint PRIMARY KEY,
     tipo varchar UNIQUE NOT NULL
 );
-
-INSERT INTO tipo_forma
-    VALUES (1, 'Circular'),
-    (2, 'Quadrado'),
-    (3, 'Retangular');
 
 CREATE TABLE pocos_succao (
     id serial PRIMARY KEY,
@@ -197,23 +134,10 @@ CREATE TABLE tipo_lancamento (
     tipo varchar UNIQUE NOT NULL
 );
 
-INSERT INTO tipo_lancamento
-    VALUES (1, 'Descarga'),
-    (2, 'Extravazamento'),
-    (3, 'Padrao de saida');
-
 CREATE TABLE tipo_manancial (
     id smallint PRIMARY KEY,
     tipo varchar UNIQUE NOT NULL
 );
-
-INSERT INTO tipo_manancial
-    VALUES (1, 'Rio'),
-    (2, 'Lago'),
-    (3, 'Acude'),
-    (4, 'Represa'),
-    (5, 'Aquifero'),
-    (6, 'Nascente');
 
 CREATE TABLE lancamentos_efluente (
     id serial PRIMARY KEY,
@@ -234,58 +158,25 @@ CREATE TABLE tipo_valvula (
     tipo varchar UNIQUE NOT NULL
 );
 
-INSERT INTO tipo_valvula
-    VALUES (1, 'Borboleta'),
-    (2, 'Esferica'),
-    (3, 'Gaveta'),
-    (4, 'Redutora de pressao'),
-    (5, 'Retencao');
-
 CREATE TABLE tipo_funcao_valvula (
     id smallint PRIMARY KEY,
     tipo varchar UNIQUE NOT NULL
 );
-
-INSERT INTO tipo_funcao_valvula
-    VALUES (1, 'Bloqueio'),
-    (2, 'Controle'),
-    (3, 'Descarga'),
-    (4, 'Redutora de pressao'),
-    (5, 'Retencao');
 
 CREATE TABLE tipo_acionamento_valvula (
     id smallint PRIMARY KEY,
     tipo varchar UNIQUE NOT NULL
 );
 
-INSERT INTO tipo_acionamento_valvula
-    VALUES (1, 'Eletrico'),
-    (2, 'Manual Chave'),
-    (3, 'Manual Volante'),
-    (4, 'Mecanico');
-
 CREATE TABLE tipo_acesso_valvula (
     id smallint PRIMARY KEY,
     tipo varchar UNIQUE NOT NULL
 );
 
-INSERT INTO tipo_acesso_valvula
-    VALUES (1, 'Caixa'),
-    (2, 'Enterrado'),
-    (3, 'Acesso livre'),
-    (4, 'PV'),
-    (5, 'Tampa T9');
-
 CREATE TABLE tipo_posicao_valvula (
     id smallint PRIMARY KEY,
     tipo varchar UNIQUE NOT NULL
 );
-
-INSERT INTO tipo_posicao_valvula
-    VALUES (1, 'Aberta'),
-    (2, 'Fechada'),
-    (3, 'Nao aplicavel'),
-    (4, 'Parcialmente aberta');
 
 CREATE TABLE valvulas (
     id serial PRIMARY KEY,
@@ -310,12 +201,6 @@ CREATE TABLE tipo_unidade_inspecao (
     id smallint PRIMARY KEY,
     tipo varchar UNIQUE NOT NULL
 );
-
-INSERT INTO tipo_unidade_inspecao
-    VALUES (1, 'Caixa de passagem'),
-    (2, 'Poco de inspecao'),
-    (3, 'Poco de visita'),
-    (4, 'Terminal de limpeza');
 
 CREATE TABLE unidades_inspecao (
     id serial PRIMARY KEY,
@@ -343,17 +228,6 @@ CREATE TABLE tipo_conexao (
     id smallint PRIMARY KEY,
     tipo varchar UNIQUE NOT NULL
 );
-
-INSERT INTO tipo_conexao
-    VALUES (1, 'Adaptador'),
-    (2, 'CAP'),
-    (3, 'Colar de tomada'),
-    (4, 'Cruzeta'),
-    (5, 'Curva'),
-    (6, 'Luva de uniao'),
-    (7, 'Reducao'),
-    (8, 'TE'),
-    (9, 'Ventosa');
 
 CREATE TABLE conexoes (
     id serial PRIMARY KEY,
