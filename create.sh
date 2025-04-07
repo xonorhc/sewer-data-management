4e9a71d34e98b8f92e2c294e0a3220753532fda6
golin <heron_golin@hotmail.com>
2025-04-07 15:12:42 -0300

    update create.sh

--- create.sh
+++ create.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 # create database
-psql -U postgres -h localhost -p 5432 -d postgres -c "create database modelo_dados_saneamento;"
+psql -U 'postgres' -h 'localhost' -p '5432' -d 'postgres' -c "create database modelo_dados_saneamento;"
 
 # create extension
-psql -U postgres -h localhost -p 5432 -d modelo_dados_saneamento -c "create extension if not exists postgis;"
+psql -U 'postgres' -h 'localhost' -p '5432' -d 'modelo_dados_saneamento' -c "create extension if not exists postgis;"
 
 # create schema
-psql -U postgres -h localhost -p 5432 -d modelo_dados_saneamento -c "create schema if not exists sistema_esgoto;"
+psql -U 'postgres' -h 'localhost' -p '5432' -d 'modelo_dados_saneamento' -c "create schema if not exists sistema_esgoto;"
 
 # find $(dirname $0)/data_definition -name "*.sql" -exec echo {} \;
 
 # create functions
-find $(dirname $0)/data_definition -name "function_*" \
-    -exec psql -h localhost -p 5432 -d modelo_dados_saneamento -U postgres -f {} \;
+find $(dirname $0)/data_definition -type f -name "function_*" \
+    -exec psql -h 'localhost' -p '5432' -d 'modelo_dados_saneamento' -U 'postgres' -f {} \;
 
 # create types/tables
-find $(dirname $0)/data_definition -name "type_*" \
-    -exec psql -h localhost -p 5432 -d modelo_dados_saneamento -U postgres -f {} \;
+find $(dirname $0)/data_definition -type f -name "type_*" \
+    -exec psql -h 'localhost' -p '5432' -d 'modelo_dados_saneamento' -U 'postgres' -f {} \;
 
 # create tables
-find $(dirname $0)/data_definition -name "table_*" -print0 | sort -z | xargs -0 -I{} \
-    psql -h 'localhos' -p '5432' -d 'modelo_dados_saneamento' -U 'postgres' -f "{}" 
+find $(dirname $0)/data_definition -type f -name "table_*" -print0 | sort -z | xargs -0 -I{} \
+    psql -h 'localhost' -p '5432' -d 'modelo_dados_saneamento' -U 'postgres' -f "{}" 
