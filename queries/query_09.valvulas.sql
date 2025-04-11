a2113f92c15223e86717d704ec99d5b03f564767
chronox <xonorhc@proton.me>
2025-04-10 23:07:17 -0300

    update query valvulas

---
+++ queries/query_09.valvulas.sql
@@ -0,0 +1,31 @@
+SET search_path TO sistema_esgoto, public;
+
+SELECT
+    v.id,
+    v.geom,
+    v.tipo,
+    tvf.tipo AS funcao,
+    v.diametro,
+    tva.tipo AS acionamento,
+    tvac.tipo AS acesso,
+    v.profundidade,
+    tvp.tipo AS posicao,
+    v.qtd_voltas_fechar,
+    v.localizacao,
+    v.observacoes
+FROM
+    valvulas AS v
+    LEFT JOIN tipo_valvula AS tv ON tv.id = v.tipo
+    LEFT JOIN tipo_valvula_funcao AS tvf ON tfv.id = v.funcao
+    LEFT JOIN tipo_valvula_acionamento AS tva ON tva.id = v.acionamento
+    LEFT JOIN tipo_valvula_acesso AS tvac ON tvac.id = v.acesso
+    LEFT JOIN tipo_valvula_posicao AS tvp ON tvp.id = v.posicao
+WHERE
+    v.geom IS NOT NULL
+    AND st_intersects (v.geom, (
+            SELECT
+                st_collect (geom) AS geom FROM redes_esgoto
+            WHERE
+                geom IS NOT NULL
+                AND situacao = 1));
+
